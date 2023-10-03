406d7038798211a91abbe6b9ef80cddee5d21660
kunila2003 <132693198+kunila2003@users.noreply.github.com>
2023-10-04 01:50:23 +0530

    Added successfully!

---
+++ script.js
@@ -0,0 +1,41 @@
+let celciusInput = document.querySelector('#celcius > input')
+let fahrenheitInput = document.querySelector('#fahrenheit > input')
+let kelvinInput = document.querySelector('#kelvin > input')
+
+let btn = document.querySelector('.button button')
+
+function roundNumber(number){
+     return Math.round(number * 100) / 100; // Fixed the Math.round syntax
+}
+/* celcius to fahrenheit and kelvin */
+celciusInput.addEventListener('input', function(){
+    let cTemp = parseFloat(celciusInput.value);
+    let fTemp = (cTemp * 9/5) + 32; // Removed the extra dot in 9/5
+    let kTemp = cTemp + 273.15;
+
+    fahrenheitInput.value = roundNumber(fTemp);
+    kelvinInput.value = roundNumber(kTemp);
+});
+/* fahrenheit to celcius and kelvin */
+fahrenheitInput.addEventListener('input', function(){
+    let fTemp = parseFloat(fahrenheitInput.value);
+    let cTemp = (fTemp - 32) * (5/9)
+    let kTemp = (fTemp - 32) * (5/9) + 273.15;
+
+    celciusInput.value = roundNumber(cTemp);
+    kelvinInput.value = roundNumber(kTemp);
+});
+/* kelvin to fahrenheit and celcius */
+kelvinInput.addEventListener('input', function(){
+    let kTemp = parseFloat(kelvinInput.value);
+    let cTemp = kTemp - 273.15
+    let fTemp = (kTemp - 273.15) * (5/9) + 32;
+
+    celciusInput.value = roundNumber(cTemp);
+    fahrenheitInput.value = roundNumber(fTemp);
+});
+btn.addEventListener('click',()=>{
+celciusInput.value = ""
+fahrenheitInput.value = ""
+kelvinInput.value = ""
+});
